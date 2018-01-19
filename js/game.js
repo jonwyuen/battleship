c32c4a113c1b18170cecb4b25715e4110ac16d3c
Jonathan Yuen <jonwyuen@gmail.com>
2018-01-18 23:21:10 -0800

    board showing up

--- js/game.js
+++ js/game.js
@@ -19,8 +19,19 @@ Example Board
 
 class Game {
   constructor() {
+  }
+
+  init() {
+    const board1 = new Board(10);
+    board1.createBoard(10)
+    const board2 = new Board(10);
+    board2.createBoard(10)
+  }
+}
+
+class Board {
+  constructor(size) {
     this.board = [];
-    this.createBoard(10);
   }
 
   createBoard(size) {
@@ -28,6 +39,18 @@ class Game {
       let subArray = new Array(size).fill(null);
       this.board.push(subArray);
     }
+
+    let board = document.createElement('table');
+    board.setAttribute('class', 'board');
+    for (let i = 0; i < size; i++) {
+      let row = document.createElement('tr');
+      board.appendChild(row);
+      for (let j = 0; j < size; j++) {
+        let cell = document.createElement('td');
+        row.appendChild(cell);
+      }
+    }
+    document.querySelector('#game').appendChild(board);
   }
 
   fire(x, y) {
@@ -36,6 +59,18 @@ class Game {
       let current = this.board[x][y];
       this.board[x][y] = null;
       // check if any of that ship is remaining
+      for (let i = 0; i < this.board.length; i++) {
+        let row = this.board[i];
+        for (let j = 0; j < row.length; i++) {
+          if (current === row[j]) {
+            this.displayMessage('Hit');
+            return;
+          }
+        }
+        this.displayMessage(`Sink ${current}`);
+      }
+    } else {
+      this.displayMessage('Miss');
     }
   }
 
@@ -43,3 +78,12 @@ class Game {
     document.querySelector('#message').innerHTML = message;
   }
 }
+
+class Ship {
+  constructor(size) {
+    this.size = size;
+  }
+}
+
+const game = new Game();
+game.init();
