f3936cfcc452a1d527a446b3651a9292626f9e11
Jonathan Yuen <jonwyuen@gmail.com>
2018-01-20 21:46:43 -0800

    more game logic

--- js/board.js
+++ js/board.js
@@ -46,11 +46,89 @@ class Board {
     document.querySelector('#game').appendChild(board);
   }
 
+  generateShips() {
+    this.ships = [];
+    for (let i = 2; i < 6; i++) {
+      this.ships.push(new Ship(i));
+    }
+  }
+
+  generateCoordinate(size) {
+    return Math.floor(Math.random() * size + 1);
+  }
+
+  // generateLocation(size) {
+  //   let randomX = this.generateCoordinate(size);
+  //   let randomY = this.generateCoordinate(size);
+  //   return this.board[randomX][randomY];
+  // }
+
+  checkBounds() {
+    
+  }
+
+  generateShipLocations(size) {
+    // random x,y in bounds and direction (right or down)
+    this.bounds = {};
+    /*
+      {
+        x: [all the y's]
+      }
+    */
+
+    this.ships.forEach(ship => {
+      let direction = Math.random() < 0.5 ? 'right' : 'down';
+      // ship.id; ship.size
+      // cannot go out of bounds
+
+      let xCoord = this.generateCoordinate(size);
+      let yCoord = this.generateCoordinate(size);
+      let location = this.board[xCoord][yCoord];
+
+      while (location) {
+        // generate random location until find one thats null
+        xCoord = this.generateCoordinate(size);
+        yCoord = this.generateCoordinate(size);
+        location = this.board[xCoord][yCoord];
+      }
+      // check if it intersects with another ship
+      // make sure all ship locations are null
+
+      this.board[xCoord][yCoord] = ship.id;
+
+      for (let i = 0; i < ship.size; i++) {
+        if (direction === 'right') {
+          
+          this.board[xCoord][++yCoord] = ship.id;
+        } else {
+          this.board[++xCoord][yCoord] = ship.id;
+        }
+      }
+    });
+  }
+
+  handleFireEvent() {
+    let board = document.querySelectorAll('.board');
+    board.forEach(board => {
+      board.addEventListener('click', e => {
+        if (e.target.matches('td')) {
+          this.fire(+e.target.dataset.x, +e.target.dataset.y);
+          e.stopPropagation();
+        }
+      });
+    });
+  }
+
   fire(x, y) {
+    let checkForWin = this.checkForWin();
+    if (checkForWin) return;
     // check if there is a ship at location
-    if (this.board[x][y]) {
+    if (this.board[x][y] === 'taken') {
+      this.displayMessage('Already taken');
+      return;
+    } else if (this.board[x][y]) {
       let current = this.board[x][y];
-      this.board[x][y] = null;
+      this.board[x][y] = 'taken';
       // check if any of that ship is remaining
       for (let i = 0; i < this.board.length; i++) {
         let row = this.board[i];
@@ -67,7 +145,23 @@ class Board {
     }
   }
 
+  checkForWin() {
+    let flag = true;
+    for (let i = 0; i < this.board.length; i++) {
+      if (!flag) break;
+      let row = this.board[i];
+      for (let j = 0; j < row.length; i++) {
+        if (row[j] !== null || row[j] !== 'taken' ) {
+          flag = false;
+          break;
+        }
+      }
+    }
+    if (flag) this.displayMessage('Win');
+    return flag;
+  }
+
   displayMessage(message) {
-    document.querySelector('#message').innerHTML = message;
+    document.querySelector('#game-message').innerHTML = message;
   }
 }
--- js/game.js
+++ js/game.js
@@ -1,11 +1,17 @@
 class Game {
-  constructor() {}
+  constructor() {
+    this.turn = Math.random() < .5 ? "Player1" : "Player2"
+  }
 
   init() {
     const board1 = new Board(10);
     board1.createBoard(10);
     const board2 = new Board(10);
     board2.createBoard(10);
+  }
+
+  switchTurns() {
+
   }
 
   reset() {}
